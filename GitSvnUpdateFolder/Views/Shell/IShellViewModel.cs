27f030568aba8edbe805ba6f61f85e9b894a8b83
celsoaf <celsoaf@gmail.com>
2014-03-26 01:46:23 +0000

    Add folder commands to taskbarItemInfo

--- GitSvnUpdateFolder/Views/Shell/IShellViewModel.cs
+++ GitSvnUpdateFolder/Views/Shell/IShellViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Practices.Prism.Commands;
 
 namespace GitSvnUpdateFolder.Views.Shell
 {
@@ -10,5 +11,10 @@ namespace GitSvnUpdateFolder.Views.Shell
         IShellView View { get; set; }
 
         double ProgressValue { get; set; }
+
+        DelegateCommand FetchAllCommand { get; set; }
+        DelegateCommand RebaseAllCommand { get; set; }
+        DelegateCommand CommitAllCommand { get; set; }
+        DelegateCommand BrowseCommand { get; set; }
     }
 }
--- GitSvnUpdateFolder/Views/Shell/ShellViewModel.cs
+++ GitSvnUpdateFolder/Views/Shell/ShellViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GitSvnUpdateFolder.Events;
+using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Prism.ViewModel;
 
@@ -10,12 +11,46 @@ namespace GitSvnUpdateFolder.Views.Shell
 {
     public class ShellViewModel : NotificationObject, IShellViewModel
     {
+        private IEventAggregator _eventAggregator;
+        private bool _running = false;
+        private string _currentPath;
+
         public ShellViewModel(IShellView view, IEventAggregator eventAggregator)
         {
             View = view;
             View.DataContext = this;
 
+            _eventAggregator = eventAggregator;
+
             eventAggregator.GetEvent<ProcessProgressEvent>().Subscribe(p => ProgressValue = p / 100);
+
+            FetchAllCommand = new DelegateCommand(
+                () => _eventAggregator.GetEvent<FetchAllEvent>().Publish(null),
+                () => !_running);
+
+            RebaseAllCommand = new DelegateCommand(
+                () => _eventAggregator.GetEvent<RebaseAllEvent>().Publish(null),
+                () => !_running);
+
+            CommitAllCommand = new DelegateCommand(
+                () => _eventAggregator.GetEvent<CommitAllEvent>().Publish(null),
+                () => !_running);
+
+            BrowseCommand = new DelegateCommand(
+                () => _eventAggregator.GetEvent<BrowseEvent>().Publish(_currentPath));
+
+            _eventAggregator.GetEvent<ProcessStartEvent>().Subscribe(obj => UpdateCommands(true));
+            _eventAggregator.GetEvent<ProcessEndEvent>().Subscribe(obj => UpdateCommands(false));
+
+            _eventAggregator.GetEvent<FolderSelectedEvent>().Subscribe(path => _currentPath = path);
+        }
+
+        private void UpdateCommands(bool running)
+        {
+            _running = running;
+            FetchAllCommand.RaiseCanExecuteChanged();
+            RebaseAllCommand.RaiseCanExecuteChanged();
+            CommitAllCommand.RaiseCanExecuteChanged();
         }
 
         public IShellView View { get; set; }
@@ -32,5 +67,10 @@ namespace GitSvnUpdateFolder.Views.Shell
                 RaisePropertyChanged(() => ProgressValue);
             }
         }
+
+        public DelegateCommand FetchAllCommand { get; set; }
+        public DelegateCommand RebaseAllCommand { get; set; }
+        public DelegateCommand CommitAllCommand { get; set; }
+        public DelegateCommand BrowseCommand { get; set; }
     }
 }
