dc9c0fc3b7d2a9311f99a3ff9b63e64d72f0b543
celsoaf <celsoaf@gmail.com>
2014-04-28 15:06:42 +0100

    Show Changed files count

---
+++ GitSvnUpdateFolder/Views/Folders/Converters/ChangesCountToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GitSvnUpdateFolder.Views.Folders.Converters
+{
+    public class ChangesCountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int && ((int)value) > 0)
+                return Brushes.DarkMagenta;
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
